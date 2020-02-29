8bea02e0b849efbb614d15e927a3542c41d313b0
amintoo <amin.daher@outlook.fr>
2020-02-29 19:55:16 +0100

    ajout du parametrage

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,8 +20,74 @@ namespace projet_UI
     /// <summary>
     /// Logique d'interaction pour MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+
+        private int taille_population;
+        public int Taille_population
+        { 
+            get { return this.taille_population; }
+            set
+            {
+               if (this.taille_population != value)
+                {
+                    this.taille_population = value;
+                    this.NotifyPropertyChanged("Taille_population");
+                }
+            }
+
+        }
+
+        private int mutation;
+        public int Mutation
+        {
+            get { return this.mutation; }
+            set {
+             
+              if (this.mutation != value)
+                {
+                    this.mutation = value;
+                    this.NotifyPropertyChanged("Mutation");
+                }
+            }
+        }
+
+        private int crossover;
+        public int Crossover
+        {
+             get { return this.crossover;  }
+             set {
+
+                if (this.crossover != value)
+                {
+                    this.crossover = value;
+                    this.NotifyPropertyChanged("Crossover");
+                }
+
+               }
+        }
+
+        private int elite;
+        public int Elite
+        {
+            get { return this.elite; }
+            set
+            {
+                if (this.crossover != value)
+                {
+                    this.crossover = value;
+                    this.NotifyPropertyChanged("Elite");
+                }
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
         private ObservableCollection<Ville> villes = new ObservableCollection<Ville>();
 
         public ObservableCollection<Ville> Villes { get => villes; set => villes = value; }
@@ -54,34 +121,17 @@ namespace projet_UI
             }
 
             Thickness t = new Thickness(x,y, 0, 0);
-             el.Margin = t;
+            el.Margin = t;
 
             // creation de la ville
             Ville town = new Ville(Convert.ToInt32(x), Convert.ToInt32(y), villename);
-            Data.getData().Insert(town);
-
-            //TODO : 
+    
             Chemin c = new Chemin();
-
+          
             List<Ville> Vlles = new List<Ville>(Villes);
-            Generation g = new Generation(10, Vlles);
-            List<Chemin> maGeneration = g.getPremierGen();
-
-
-            /*
-            foreach(Chemin c in maGeneration)
-            {
-                Console.WriteLine(c.ToString());
-                Console.WriteLine(c.Score);
-            } 
+          
             Villes.Add(town);
             canva.Children.Add(el);
-            
-            */
-
-            c.GetFirstGeneration(10, Vlles);
-            Console.WriteLine(c.GetFirstGeneration(10, Vlles));
-
 
 
         }
