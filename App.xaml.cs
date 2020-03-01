53d37cd080ffd41ef4f01ae7fb3ee707481f9cd0
amintoo <amin.daher@outlook.fr>
2020-03-01 17:17:51 +0100

    fix bug

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace projet_UI
+namespace ApplicationWPF
 {
     /// <summary>
     /// Logique d'interaction pour App.xaml
